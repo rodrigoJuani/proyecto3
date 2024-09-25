193330110b0afdeb5334b07d5b4b62334f733ebb
MrRobot <juaniquinapokar@gmail.com>
2024-09-25 12:04:42 -0400

    Separando formularios de los que tienen cuenta y los que no

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,22 +1,29 @@
+import React,{useState} from 'react'
 import React from 'react'
 import './Login.css'
 import assets from 'C:/Users/pc/Desktop/proyecto3/src/assets/assets'
 const Login=()=>{
+
+          const[currState,setCurrState]=useState("Sign up");
+
     return(
         <div className='login'>
           <img src={assets.logo_big} alt="" className="logo" />
           <form action="" className="login-form">
-            <h2>sign Up</h2>
-            <input type="text" placeholder="username" className="form-input" required/>
+            <h2>{currState}</h2>
+            {currState==="Sign up"? <input type="text" placeholder="username" className="form-input" required/>:null}
             <input type="email" placeholder='Email address' className="form-input" required/>
             <input type="password" placeholder='password' className="form-input" required/>
-            <button type='submit'>Sign Up</button>
+            <button type='submit'>{currState==="Sign up"?"Create account":"Login now"}</button>
             <div className="login-term">
               <input type='checkbox'/>
               <p>Agree to the terms of use & privacy policy</p>
             </div>
             <div className="login-forgot">
-              <p className="login-toggle">Already have an account <span>click here</span></p>
+              {
+                currState==="Sig up"
+                ?<p className="login-toggle">Already have an account <span onClick={()=>setCurrState("Login")}>Login here</span></p>:<p className="login-toggle">Create an account<span onClick={()=>setCurrState("Sign up")}>click here</span></p>
+              }
             </div>
           </form>
         </div>
