af9b8a953d9aa5e7f1be53103a913c063198c73a
MrRobot <juaniquinapokar@gmail.com>
2024-10-08 19:33:21 -0400

    parte del chat8

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import './RightSidebar.css'
+import assets from "../../assets/assets"
 const RightSidebar=()=>{
-    return(<div>
+    return(
+    <div className="rs">
+        <div className="rs-profile">
+            <img src={assets.profile_img} alt=""/>
+            <h3>Richard Sanford<img src={assets.green_dot} className="dot" alt=""/></h3>
+            <p>HEy, there u am richar sanford using chat app</p>
+        </div>
     </div>)
 }
+
 export default RightSidebar
