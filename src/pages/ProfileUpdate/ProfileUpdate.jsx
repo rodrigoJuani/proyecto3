e0969d929493047862e2cdeecb8549e516895fcc
MrRobot <juaniquinapokar@gmail.com>
2024-10-13 15:00:08 -0400

    parte del chat13

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
 const ProfileUpdate=()=>{
     return(
-        <div>
-         <h1></h1>
+        <div className='profile'>
+            <div className="profile-container">
+                <form action=''>
+                    <h3>Profile details</h3>
+                    <label htmlFor='avatar'>
+                        <input type='file' id='avatar' accept='.png, .jpg, .jpeg' hidden/>
+                        <img src={assets.avatar_icon} alt=""/>
+                        upload profile image
+                    </label>
+                    <input type="text" placeholder='Your name' required />
+                    <textarea placeholder='Write profile bio' required></textarea>
+                    <button type='submit'>Save</button>
+                </form>
+                <img src={assets.logo_icon} alt=''/>
+            </div>
         </div>
     )
 }
