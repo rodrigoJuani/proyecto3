0ba8bccb0cb2d800a2dbea716a5af8dceafb6869
MrRobot <juaniquinapokar@gmail.com>
2024-10-20 17:34:05 -0400

    correcion0

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,33 @@
+import{doc, getDoc} from "firebase/firestore";
+import { createContext,useState } from "react";
+import{db} from "../config/firebase";
+
+export const AppContext=createContext();
+
+const AppContextProvider=(props)=>{
+
+    const[useData,setUserData]=useState(null);
+    const[chatData,setChatData]=useState(null);
+
+    const loadUserData=async(uid)=>{
+        try{
+            const userRef=doc(db,'users',uid);
+            const userSnap=await getDoc(userRef);
+            console.log(userSnap)
+        }catch(error){
+        }
+    }
+
+    const value={
+        useData,setUserData,
+        chatData,setChatData,
+        loadUserData
+    }
+    return(
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
+
+export default AppContextProvider
