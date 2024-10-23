3efcb121fa971cdcc879786be758ebabee786645
MrRobot <juaniquinapokar@gmail.com>
2024-10-22 21:19:00 -0400

    poniendo comentario en el chat0

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -8,8 +8,6 @@ const storageRef = ref(storage, `images/${date.now()+file.name}`);
 const uploadTask = uploadBytesResumable(storageRef, file);
 uploadTask.on('state_changed', 
   (snapshot) => {
-    // Observe state change events such as progress, pause, and resume
-    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
     const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
     console.log('Upload is ' + progress + '% done');
     switch (snapshot.state) {
@@ -28,7 +26,7 @@ uploadTask.on('state_changed',
     // Handle successful uploads on complete
     // For instance, get the download URL: https://firebasestorage.googleapis.com/...
     getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-      console.log('File available at', downloadURL);
+      resolve(downloadURL)
     });
   }
 );
--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,8 +1,25 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import './ProfileUpdate.css'
 import assets from '../../assets/assets'
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../../config/firebase';
+import { doc, getDoc } from 'firebase/firestore';
 const ProfileUpdate=()=>{
     const [image,setImage]=useState(false);
+    const[name,setName]=useState("");
+    const[bio,setBio]=useState("");
+    const[uid,setUid]=useState("");
+
+    useEffect(()=>{
+        onAuthStateChanged(auth,async(user)=>{
+            if(user){
+                setUid(user.uid)
+                const docRef=doc(db,"users",user.uid);
+                const docSnap=getDoc(docRef);
+            }
+        })
+    },[])
+
     return(
         <div className='profile'>
             <div className="profile-container">
@@ -13,8 +30,8 @@ const ProfileUpdate=()=>{
                         <img src={image? URL.createObjectURL(image) : assets.avatar_icon} alt=""/>
                         upload profile image
                     </label>
-                    <input type="text" placeholder='Your name' required />
-                    <textarea placeholder='Write profile bio' required></textarea>
+                    <input onChange={(e)=>setName(e.target.value)} value={name} type="text" placeholder='Your name' required />
+                    <textarea onChange={(e)=>setBio(e.target.bio)} value={bio} placeholder='Write profile bio' required></textarea>
                     <button type='submit'>Save</button>
                 </form>
                 <img  className="profile-pic" src={image? URL.createObjectURL(image) : assets.logo_icon} alt=''/>
