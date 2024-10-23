ba99df1a094980de9121d264f8391ad8a1af56d3
MrRobot <juaniquinapokar@gmail.com>
2024-10-22 21:39:25 -0400

    poniendo comentario en el chat1

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -2,20 +2,46 @@ import React,{useEffect, useState} from 'react'
 import './ProfileUpdate.css'
 import assets from '../../assets/assets'
 import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '../../config/firebase';
+import { auth ,db} from '../../config/firebase';
 import { doc, getDoc } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 const ProfileUpdate=()=>{
+
+    const navigate=useNavigate();
     const [image,setImage]=useState(false);
     const[name,setName]=useState("");
     const[bio,setBio]=useState("");
     const[uid,setUid]=useState("");
+    const [prevImage,setPrevImage]=useState("");
+
+    const profileUpdate=async(event)=>{
+        event.preventDefault();
+        try{
+            if(!prevImage && image){
+                toast.error("Upload profile picture")
+            }
+        }catch(error){
+        }
+    }
 
     useEffect(()=>{
         onAuthStateChanged(auth,async(user)=>{
             if(user){
                 setUid(user.uid)
                 const docRef=doc(db,"users",user.uid);
-                const docSnap=getDoc(docRef);
+                const docSnap=await getDoc(docRef);
+                if(docSnap.data().name){
+                    setName(docSnap.data().name);
+                }
+                if(docSnap.data().bio){
+                    setBio(docSnap.data().bio);
+                }
+                if(docSnap.data().avatar){
+                    setPrevImage(docSnap.data().avatar)
+                }
+            }else{
+                navigate('/')
             }
         })
     },[])
@@ -23,7 +49,7 @@ const ProfileUpdate=()=>{
     return(
         <div className='profile'>
             <div className="profile-container">
-                <form action=''>
+                <form onSubmit={profileUpdate}>
                     <h3>Profile details</h3>
                     <label htmlFor='avatar'>
                         <input onChange={(e)=>setImage(e.target.files[0])} type='file' id='avatar' accept='.png, .jpg, .jpeg' hidden/>
