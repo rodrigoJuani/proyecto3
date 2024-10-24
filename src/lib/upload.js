a5f77b6c963d1c57134e0e5a7c4b4ff106f6ee24
MrRobot <juaniquinapokar@gmail.com>
2024-10-24 16:45:49 -0400

    profileDetails0

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -7,6 +7,25 @@ const upload =async(file)=>{
 
     const uploadTask = uploadBytesResumable(storageRef, file);
 
+    upload.on('state_changed',
+        (snapshot)=>{
+            const progress=(snapshot.bytesTransferred / snapshot.totalBytes)*100;
+            console.log('Upload.is' +progress+'% donde');
+            switch(snapshot.state){
+                case'pause':
+                            console.log('Upload is paused');
+                            break;
+                case 'running':
+                            console.log('Upload is running');
+                            break;
+            }
+        },
+        (error)=>{
+        },
+        ()=>{
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>)
+        }
+    )
     return new Promise((resolve,reject)=>{
     })
 }
