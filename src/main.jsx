51ae76533c977347509d7802671577757f524677
MrRobot <juaniquinapokar@gmail.com>
2024-10-27 16:31:54 -0400

    correccion0

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import ReactDOOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import AppContextProvider from './context/AppContext.jsx'
 
-ReactDOOM.createRoot(document.getElementById('root')).render(//ReactDOOM
+ReactDOM.createRoot(document.getElementById('root')).render(//ReactDOOM
     <BrowserRouter>
       <AppContextProvider>
         <App />
