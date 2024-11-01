5ab0d97e06b8979de1e81b785a6134a6980660c1
MrRobot <juaniquinapokar@gmail.com>
2024-10-31 21:25:34 -0400

    Welcome7

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -10,7 +10,7 @@ const ChatBox=()=>{
     return chatUser ? (
     <div className="chat-box">
         <div className="chat-user">
-            <img src={assets.profile_img} alt=''/>
+            <img src={chatUser.userData.avatar} alt=''/>
             <p>Richard Sanford <img className="dot" src={assets.green_dot} alt=''/></p>
             <img src={assets.help_icon} className="help" alt=""/>
         </div>
