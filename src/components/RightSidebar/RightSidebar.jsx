ee769f8ec45fc926899f09b68af1a523827bd2b0
MrRobot <juaniquinapokar@gmail.com>
2024-11-05 09:36:12 -0400

    mensajesEnChat35

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -9,7 +9,7 @@ const RightSidebar=()=>{
     <div className="rs">
         <div className="rs-profile">
             <img src={chatUser.userData.avatar} alt=""/>
-            <h3>Richard Sanford<img src={assets.green_dot} className="dot" alt=""/></h3>
+            <h3>{chatUser.userData.name}<img src={assets.green_dot} className="dot" alt=""/></h3>
             <p>HEy, there u am richar sanford using chat app</p>
         </div>
         <hr/>
