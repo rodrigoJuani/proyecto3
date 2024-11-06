45883103365a8404ae590d957087991a327283e1
MrRobot <juaniquinapokar@gmail.com>
2024-11-06 15:09:07 -0400

    mensajesEnChat55

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -100,7 +100,7 @@ const ChatBox=()=>{
     <div className="chat-box">
         <div className="chat-user">
             <img src={chatUser.userData.avatar} alt=''/>
-            <p>{chatUser.userData.name} {Date.now-chatUser.userData.lastSeen} </p>
+            <p>{chatUser.userData.name} {Date.now-chatUser.userData.lastSeen<=} </p>
             <img src={assets.help_icon} className="help" alt=""/>
         </div>
 
