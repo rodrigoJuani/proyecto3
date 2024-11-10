66b3b6a68d53c8a52b7ac85bbb8d21654a7152c3
MrRobot <juaniquinapokar@gmail.com>
2024-11-10 18:33:58 -0400

    mensajesEnChat127

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -22,7 +22,7 @@ const Login=()=> {
   return (
     <div className='login'>
       <img src={assets.logo_big} alt="" className="logo" />
-      <form onSubmit={onSubmitHandler} action="" className="login-form">
+      <form onSubmit={onSubmitHandler} className="login-form">
         <h2>{currState}</h2>
         {currState === "Sign up" ? <input onChange={(e) => setUserName(e.target.value)} value={userName} type="text" placeholder="username" className="form-input" required /> : null}
 
@@ -34,8 +34,12 @@ const Login=()=> {
           <p>Agree to the terms of use & privacy policy</p>
         </div>
         <div className="login-forgot">
-          {currState === "Sign up"
-            ? <p className="login-toggle">Already have an account <span onClick={() => setCurrState("Login")}>Login here</span></p> : <p className="login-toggle">Create an account<span onClick={() => setCurrState("Sign up")}>click here</span></p>}
+          {
+          currState === "Sign up"
+            ? <p className="login-toggle">Already have an account <span onClick={() => setCurrState("Login")}>Login here</span></p> 
+            : <p className="login-toggle">Create an account<span onClick={() => setCurrState("Sign up")}>click here</span></p>
+          }
+          {currState==="Login"?<p className="login-toggle">Forgot Password?<span onClick={() => setCurrState("Sign up")}>reset here</span></p>:null}
         </div>
       </form>
     </div>
